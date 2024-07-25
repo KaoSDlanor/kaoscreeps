f762763030fdf30dd84f29bd20e9f6038bcbb5c9
KaoSDlanor <KaoSDlanor@gmail.com>
2024-07-25 13:52:24 +0000

    Added some comments

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,10 +1,15 @@
 const CONSTANTS = {
+	/** how often to read persistent room data */
   lookupInterval : 10,
+	/** how long to delay persistent room data reading to avoid high CPU usage in a single tick */
   lookupOffset   : 0,
 
+	/** how often to spawn new creeps */
   spawnInterval  : 5,
+	/** how long to delay spawning new creeps to avoid high CPU usage in a single tick */
   spawnOffset    : 1,
 
+	/** base screeps room dimensions */
   roomSize : { x : 50, y : 50 },
 } as const;
 
